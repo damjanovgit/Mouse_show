d5f69eeba8beca2aeb9984cbab88f8b58b487736
Slaven Smiljanic <36378534+smiljanic997@users.noreply.github.com>
2020-04-05 18:14:36 +0200

    alignment fix

--- PIO_v2/urs_1/led_speed_show.vhd
+++ PIO_v2/urs_1/led_speed_show.vhd
@@ -15,16 +15,16 @@ begin
   process(led_speed)
 	begin
 	case to_integer(signed(led_speed)) is
-		when -64 to -4 => led_out <= "1100000000";
+		when -64 to -4 => led_out <= 	 "1100000000";
 		when -3 => led_out <= 	  	 "0110000000";
 		when -2 => led_out <= 	  	 "0011000000";
 		when -1 => led_out <= 	  	 "0001100000"; 
-		when 0 => led_out <= 		  	 "0000110000"; 
-		when 1 => led_out <= 		  	 "0000011000"; 
-		when 2 => led_out <= 	  	  	 "0000001100"; 
-		when 3 => led_out <= 		  	 "0000000110"; 
+		when 0 => led_out  <= 		 "0000110000"; 
+		when 1 => led_out  <= 		 "0000011000"; 
+		when 2 => led_out  <= 	  	 "0000001100"; 
+		when 3 => led_out  <= 		 "0000000110"; 
 		when 4 to 64 => led_out <= 	 "0000000011";
-		when others => led_out <=  	 "0000110000";
+		when others => led_out  <=  	 "0000110000";
 	end case;
   end process;
 end architecture rtl;
